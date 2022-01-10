fd3b743453948ace3ea5665cad9d641efd97e9e3
nfahmed11 <nfahmed11@gmail.com>
2022-01-10 14:39:05 -0600

    finished

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-console.log("connected");
-
 // function for which key was pressed down
+
 document.addEventListener("keydown", function (e) {
   const eCode = e.code;
   const audio = document.querySelector(`audio[code=${eCode}]`);
+  const div = document.querySelector(`div[code=${eCode}]`);
+
+  div.classList.add("clicked");
+  if (!audio) return;
+  audio.currentTime = 0;
   audio.play();
-  console.log(audio);
+
+  div.addEventListener("transitionend", () => {
+    div.classList.remove("clicked");
+  });
 });
